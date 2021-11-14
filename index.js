2cbefc712043bfdd456698c816cd0a819b7f6f2b
Jimi <50575362+andorjimi@users.noreply.github.com>
2021-11-14 22:45:21 +0800

    built function to make randomise the dice on page refresh.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 // console.log(randomNumber1);
+
+const player1 = document.querySelector(".img1");
+const player2 = document.querySelector(".img2");
+
+randomDice(player1);
+randomDice(player2);
+
+function randomDice(element) {
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    element.src = "images/dice" + randomNumber1 + ".png";
+};
