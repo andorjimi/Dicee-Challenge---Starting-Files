09295ca289b5d0a9807c2572977d849fcb57c4f8
Jimi <50575362+andorjimi@users.noreply.github.com>
2021-11-15 19:06:17 +0800

    Current Working file

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+// var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-// console.log(randomNumber1);
+// const player1 = document.querySelector('.img1');
+// const player2 = document.querySelector('.img2');
 
-const player1 = document.querySelector(".img1");
-const player2 = document.querySelector(".img2");
+// let player1Dice = randomDice(player1);
+// let player2Dice = randomDice(player2);
 
-randomDice(player1);
-randomDice(player2);
+// console.log(player1Dice);
+// console.log(player2Dice);
 
-function randomDice(element) {
-    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-    element.src = "images/dice" + randomNumber1 + ".png";
-};
+// function randomDice(element) {
+// 	var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+// 	element.src = 'images/dice' + randomNumber1 + '.png';
+// }
+
+let player1Dice = randomDice();
+let player2Dice = randomDice();
+
+function randomDice() {
+	var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+	return randomNumber1;
+}
+
+// This Function doesn't work!!
+function updateScreen(playerDiceNumber, img) {
+	document.querySelector(img);
+	element.src = 'images/dice' + playerDiceNumber + '.png';
+}
+updateScreen(player1Dice, '.img1');
+
+console.log(player1Dice);
+console.log(player2Dice);
